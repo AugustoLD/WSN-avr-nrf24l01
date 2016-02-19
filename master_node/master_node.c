e54fdde748bff6664d156f1e9d64c47a548726f2
Augusto Lopez Dantas <aldantas@protonmail.com>
2016-02-19 11:47:02 -0200

    Makefiles reduced

--- master_node/master_node.c
+++ master_node/master_node.c
@@ -9,7 +9,7 @@ uint8_t NRF = PD2;
 uint8_t generated_addres[ADDR_SIZE] = "nrf00";
 volatile uint16_t brightness;
 
-void mount_address_package(void)
+void mount_and_send_address_package(void)
 {
 	uint8_t temp[MAX_MSG_SIZE];
 	uint8_t new_message_size;
@@ -18,6 +18,7 @@ void mount_address_package(void)
 	message[payload_size - 3] = 'A';
 	memcpy(&message[payload_size - 2], generated_addres, ADDR_SIZE);
 	new_message_size = payload_size + (ADDR_SIZE - 2);
+	USART_print_string("\n");
 	USART_transmit_byte_array(message, new_message_size);
 	generated_addres[4] += 1;
 	ignore_next_pkg = 1;
